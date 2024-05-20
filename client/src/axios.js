4b7ff82ef33a02eecdaf4e0b98a1710b0350c292
Aleksandr Kornevskyi <kornevskiy99@gmail.com>
2024-05-20 15:24:06 +0300

    build: update base url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { BASE_URL } from './constants';
 
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_BACKEND_URL,
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,6 @@
 import brands from '../brands.json';
 
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL;
+
 export const BRANDS = brands;
